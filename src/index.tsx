4a32ec39ac86185b5c0424556344c3f2be98d971
AndriyIt <andriy523625@gmail.com>
2021-12-01 12:50:38 +0200

    change branch

--- src/index.tsx
+++ src/index.tsx
@@ -4,21 +4,13 @@ import { Provider } from 'react-redux';
 
 import { store } from './redux/store';
 
-<<<<<<< HEAD:src/index.js
-ReactDOM.render (
-=======
 import App from './App';
 
 ReactDOM.render(
->>>>>>> TypeScript:src/index.tsx
     <React.StrictMode>
         <Provider store={store}>
             <App/>
         </Provider>
     </React.StrictMode>,
-<<<<<<< HEAD:src/index.js
-    document.getElementById ('root'),
-=======
-    document.getElementById('root'),
->>>>>>> TypeScript:src/index.tsx
+    document.getElementById ('root')
 );
--- src/styled/Photos.ts
+++ src/styled/Photos.ts
@@ -1,15 +1,9 @@
 import styled from 'styled-components';
 
 const StyledPhotos = styled.div`
-<<<<<<< HEAD:src/styled/Photos.js
 	display: flex;
 	flex-wrap: wrap;
 	justify-content: space-between;
-=======
-    display: flex;
-    flex-wrap: wrap;
-    justify-content: space-between;
->>>>>>> TypeScript:src/styled/Photos.ts
 `;
 
 export default StyledPhotos;
