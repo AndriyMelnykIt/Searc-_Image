84a40e64feeb500551a53417b039b4505fca36f0
AndriyIt <andriy523625@gmail.com>
2021-12-02 13:33:42 +0200

    added test for Header component

---
+++ src/tests/Header.test.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { configure, shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import { Provider } from 'react-redux';
+
+import { Header } from '../Components/index';
+
+import { store } from '../redux/store';
+
+configure({adapter: new Adapter()});
+
+let wrapper: any;
+
+describe('Header', () => {
+
+    beforeAll( ()=> {
+        wrapper = shallow(<Provider store={store}> <Header /></Provider>);
+    })
+
+    it('should take a snapshot', () => {
+        expect(wrapper).toMatchSnapshot();
+    });
+
+})
